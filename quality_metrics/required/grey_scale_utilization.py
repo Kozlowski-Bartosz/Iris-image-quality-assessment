7e11826097124bb5dbdea918060623507fd4c223
Kozlowski-Bartosz <kozlowski.bartosz@protonmail.com>
2023-10-25 20:40:21 +0200

    Split GreyScale into more functions

--- quality_metrics/required/grey_scale_utilization.py
+++ quality_metrics/required/grey_scale_utilization.py
@@ -14,16 +14,18 @@ class GreyScale:
     def get_grey_level_probability(self, level):
         px_in_level = (self.image[self.image == level])
         return len(px_in_level) / self.image.size
+    
+    def calculate_entropy_for_level(self, level):
+        p = self.get_grey_level_probability(level)
+        return -p * math.log(p, 2) if p > 0 else 0
         
     def calculate_entropy_in_bits(self):
         entropy = 0
         grey_level_count = self.get_grey_level_count_in_image()
         
-        
         for level in range(grey_level_count):
-            p = self.get_grey_level_probability(level)
-            entropy += -p * math.log(p, 2) if p > 0 else 0
-            
+            entropy += self.calculate_entropy_for_level(level)
+        
         return entropy
     
     def is_entropy_above_thresh(self, entropy):
