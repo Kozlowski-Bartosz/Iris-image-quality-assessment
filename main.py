6238615fa06b65b91e1798ab8d04e2170988d93f
Kozlowski-Bartosz <kozlowski.bartosz@protonmail.com>
2023-10-25 20:20:18 +0200

    Implement grey_scal_utilization

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from sys import argv
 from sys import exit as sysexit
 
 import cv2
-# import quality_metrics.required.grey_scale_utilization
+import numpy as np
+import quality_metrics.required.grey_scale_utilization as gs
+import quality_metrics.required.iris_segmentation as iseg
 # import quality_metrics.required.iris_pupil_cocentricity
 # import quality_metrics.required.iris_sclera_contrast
 # import quality_metrics.required.iris_pupil_contrast
@@ -13,15 +15,19 @@ import cv2
 # import quality_metrics.required.sharpness
 # import quality_metrics.required.usable_iris_area
 
-
 if __name__ == "__main__":
     if len(argv) == 1:
         print("Usage: python main.py <image path>")
         sysexit()
 
-    img = cv2.imread(argv[1])
+    img = cv2.imread(argv[1], cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (320, 240))
     cv2.imshow("Image", img)
+    
+    grey = gs.GreyScale(img)
+    entropy = grey.calculate_entropy_in_bits()
+    print("Entropy: {}".format(entropy))
+    print("Is entropy above threshold? {}".format(grey.is_entropy_above_thresh(entropy)))
 
 
     cv2.waitKey(0)
--- quality_metrics/required/grey_scale_utilization.py
+++ quality_metrics/required/grey_scale_utilization.py
@@ -1,20 +1,27 @@
 import math
 import cv2
 
-THRESH = 6.0
-
 class GreyScale:
+    
+    THRESH = 6.0
+    
     def __init__(self, image):
         self.image = image
         
-    def calculate_entropy(self):
+    def get_grey_level_count_in_image(self):
+        return len(set(self.image.flatten()))
+        
+    def calculate_entropy_in_bits(self):
         entropy = 0
-        # function here
+        grey_level_count = self.get_grey_level_count_in_image()
+        px_in_img = self.image.size
+        
+        for level in range(grey_level_count):
+            px_in_level = (self.image[self.image == level])
+            p = len(px_in_level) / px_in_img
+            entropy += -p * math.log(p, 2) if p > 0 else 0
+            
         return entropy
     
-    def is_entropy_above_thresh(self, THRESH):
-        entropy = self.calculate_entropy()
-        if entropy >= THRESH:
-            return True
-        else:
-            return False
+    def is_entropy_above_thresh(self, entropy):
+        assert entropy >= self.THRESH, "Entropy is below threshold."
